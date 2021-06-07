9ee314637e9f9dad8b2b9463bd496f536ecf5f78
Abhishes <abhishes007@gmail.com>
2021-06-07 22:39:09 +0530

    feat(redux-shop): implmenting actions and types file for shop and using them in reducer for update_collections.

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,18 @@
 import SHOP_DATA from './shop.data'
 
+import ShopActionTypes from './shops.types';
+
 const INITIA_STATE = {
     collections: SHOP_DATA
 }
 
 const shopReducer = (state = INITIA_STATE, action) => {
     switch (action.type) {
+        case ShopActionTypes.UPDATE_COLLECTIONS:
+            return {
+                ...state,
+                collections: action.payload
+            }
         default:
             return state;
     }
