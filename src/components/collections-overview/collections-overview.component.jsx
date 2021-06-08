ba840549db7796c7ab99a01d71e732d1cc019eb6
Abhishes <abhishes007@gmail.com>
2021-06-08 22:08:34 +0530

    refactor(components-collections): restyling collection-preview and overview components with javascript style files.

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../preview-collection/collection-preview.component';
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selector.js'
+import CollectionPreview from '../collection-preview/collection-preview.component';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector.js';
 
-import './collections-overview.styles.scss';
+import { CollectionsOverviewContainer } from './collections-overview.styles';
 
-const CollectionOverview = ({ collections }) => (
-    <div className='collections-overview'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-    </div>
-)
+const CollectionsOverview = ({ collections }) => (
+  <CollectionsOverviewContainer>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </CollectionsOverviewContainer>
+);
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollectionsForPreview
-})
+  collections: selectCollectionsForPreview
+});
 
-export default connect(mapStateToProps)(CollectionOverview);
+export default connect(mapStateToProps)(CollectionsOverview);
